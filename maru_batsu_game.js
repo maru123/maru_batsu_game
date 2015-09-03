dfba2ad2625990e7ab224c0ac074381128997805
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-09-03 12:20:23 +0900

    refactoring

--- maru_batsu_game.js
+++ maru_batsu_game.js
@@ -24,7 +24,7 @@ MaruBatsu.prototype.reset = function(lengthOfSide){
   this.winner = "";
 
   //1辺の方眼の数
-  lengthOfSide;
+  this.lengthOfSide = lengthOfSide;
 
   //方眼の列と行を作成
   for (var row = 1 ; row <= this.lengthOfSide ; row++){
@@ -34,45 +34,37 @@ MaruBatsu.prototype.reset = function(lengthOfSide){
   }
 
   //勝ちkeyの組み合わせを作成
-  LeftDiagonalLine = [];
-  RightDiagonalLine = [];
-
-  for (var row = 1 ; row <= lengthOfSide ; row++){
-
+  //行列
+  for (var row = 1 ; row <= this.lengthOfSide ; row++){
     rowLines = [];
     columnLines = [];
-
-    for (var column = 1 ; column <= lengthOfSide ; column++){
+    for (var column = 1 ; column <= this.lengthOfSide ; column++){
       rowLines.push(row.toString() + column.toString());
       columnLines.push(column.toString() + row.toString());
     }
-    //rows
     this.breakableLines.push(rowLines);
-    //columns
     this.breakableLines.push(columnLines);
-    //LeftDiagonal
+  }
+  //斜め
+  LeftDiagonalLine = [];
+  RightDiagonalLine = [];
+  for (var row = 1 , column = this.lengthOfSide; row <= this.lengthOfSide ; row++,column--){
+    RightDiagonalLine.push(row.toString() + column.toString())
     LeftDiagonalLine.push(row.toString() + row.toString())
   }
-
-  this.breakableLines.push(LeftDiagonalLine);
-
-  //RightDiagonal
-  for (var row = 1 , column = lengthOfSide; row <= lengthOfSide ; row++,column--){
-    RightDiagonalLine.push(row.toString() + column.toString())
-  }
   this.breakableLines.push(RightDiagonalLine);
-
-  //debug
-  return this.squares;
+  this.breakableLines.push(LeftDiagonalLine);
 };
 MaruBatsu.prototype.isBreake = function(){
-
+  //勝ちkeyの組み合わせ毎にvalueの配列を取得
   for (var i = 0 ; i < this.breakableLines.length ; i++){
     var arr = [], name ;
-    for (var j = 0 ; j < 3 ; j++){
+    for (var j = 0 ; j < this.lengthOfSide ; j++){
       arr.push(this.squares[this.breakableLines[i][j]]);
     }
+    //配列の重複を削除
     name = arr.filter(function (x, i, self) {return self.indexOf(x) === i;});
+    //判定
     if (name.length === 1 && name[0] != null) {
       this.winner = name[0];
       return true;
